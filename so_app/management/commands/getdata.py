3c43906b4fb28c988cd10563a0ea8b77212a99f6
Vipul Sharma <vipul.sharma20@gmail.com>
2016-06-12 14:05:10 +0530

    added custom django-admin command: getdata

---
+++ so_app/management/commands/getdata.py
@@ -0,0 +1,14 @@
+import os
+from django.core.management.base import BaseCommand
+from so_app.script import StackExchangeInfo
+
+
+class Command(BaseCommand):
+    help = "Get data and update models"
+
+    def handle(self, *args, **options):
+        try:
+            so = StackExchangeInfo()
+            so.get_tag_info()
+        except Exception as e:
+            raise e
